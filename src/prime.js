4159bf058eb72e490294f53559b4910e228d9d5f
Cathal357 <85661615+Cathal357@users.noreply.github.com>
2026-09-21 18:48:48 +0100

    Added prime number generation

---
+++ src/prime.js
@@ -0,0 +1,36 @@
+// Checks whether a number is prime.
+function isPrime(number) {
+    if (number < 2) {
+        return false;
+    }
+
+    for (let divisor = 2; divisor <= Math.sqrt(number); divisor++) {
+        if (number % divisor === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Generates the first N prime numbers.
+function generatePrimes(n) {
+    const primes = [];
+    let number = 2;
+
+    while (primes.length < n) {
+        if (isPrime(number)) {
+            primes.push(number);
+        }
+
+        number++;
+    }
+
+    return primes;
+}
+
+//Makes functions available outside of prime.js
+module.exports = {
+    isPrime,
+    generatePrimes
+};
